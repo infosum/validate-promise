20c38b5747e71d6e54afa0a0d09cdcbad910a4e9
Rob Clayburn <rob@cognitivelogic.com>
2019-07-29 22:41:55 +0100

    bump version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -17,16 +17,18 @@ export { default as isUploaded } from './rules/isUploaded';
 export declare type ArgFunc<T extends object, R> = (value: string, row: T) => R;
 export declare type MsgFunc<T extends object, A = any> = (value: string, row: T, arg?: A | ArgFunc<T, A>) => string;
 export declare type ValidationPromise<T extends object = object, A = any> = (value: string | string[] | Record<string, number>, row: T, msg: MsgFunc<T, A>, arg?: A | ArgFunc<T, A>) => Promise<string | void>;
+interface IAPromise<T extends object = object> {
+    rule: ValidationPromise<T>;
+    arg?: (value?: string, row?: T) => any;
+    msg?: (value?: string, row?: T, arg?: any) => string;
+    condition?: (value?: string, row?: T) => boolean;
+}
 export interface Validation<T extends object = object> {
-    promises: {
-        rule: ValidationPromise<T>;
-        arg?: (value?: string, row?: T) => any;
-        msg?: (value?: string, row?: T, arg?: any) => string;
-    }[];
+    promises: IAPromise<T>[];
     key: string | string[];
     keys?: Array<string | string[]>;
     msg?: (value?: string, row?: T, arg?: any) => string;
 }
-declare const validate: (contract: Validation<object>[], data: Object) => Promise<boolean | Object>;
+declare const validate: <T extends object = Object>(contract: Validation<T>[], data: any) => Promise<boolean | Object>;
 export default validate;
 export { after, before, blacklist, email, equals, equalto, float, greaterthan, int, lessthan, notEquals, regex, required, whitelist, };
